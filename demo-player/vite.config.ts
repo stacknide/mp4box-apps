223bc69069d14d55fa3247775fce771fe18c7614
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2024-03-06 09:56:55 +0530

    feat: Transcoder groundwork

--- demo-player/vite.config.ts
+++ demo-player/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { Connect, PluginOption, defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const crossOriginIsolationMiddleware: Connect.NextHandleFunction = function (
+  _,
+  response,
+  next
+) {
+  response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+};
+
+// Plugin credits: https://github.com/vitejs/vite/issues/9864#issuecomment-1232047847
+const crossOriginIsolation: PluginOption = {
+  name: "cross-origin-isolation",
+  configureServer: (server) => {
+    server.middlewares.use(crossOriginIsolationMiddleware);
+  },
+  configurePreviewServer: (server) => {
+    server.middlewares.use(crossOriginIsolationMiddleware);
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crossOriginIsolation],
+});
--- lib/transcode-plugin/src/Transcoder.ts
+++ lib/transcode-plugin/src/Transcoder.ts
@@ -1,8 +1,88 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { toBlobURL } from "@ffmpeg/util";
+import { nanoid } from "nanoid";
 
+type TDist = "umd" | "esm";
+type TConfig = {
+  enableLogs: boolean;
+  dist: TDist;
+  enableMultiThreading: boolean;
+};
+type TTranscode = {
+  inputData: Uint8Array | string;
+  id: string;
+  timeout?: number;
+  ext?: string;
+};
 export class Transcoder {
   public ffmpeg: FFmpeg;
-  constructor() {
+  private isReady = false;
+
+  defaultConfig: TConfig = {
+    enableLogs: false,
+    dist: "umd",
+    enableMultiThreading: false,
+  };
+  private cfg = this.defaultConfig;
+
+  constructor(config?: TConfig) {
+    if (config) this.cfg = { ...this.defaultConfig, ...config };
+
+    if (this.cfg.enableMultiThreading) {
+      const refer = `Refer https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer#security_requirements"`;
+      if (!window.crossOriginIsolated) {
+        throw new Error(
+          `Cross-origin isolation is required to enable multi-threading. ${refer}`
+        );
+      }
+
+      if (!window.isSecureContext) {
+        throw new Error(
+          `Secure context is required to enable multi-threading. ${refer}`
+        );
+      }
+    }
+
     this.ffmpeg = new FFmpeg();
   }
+
+  load = async () => {
+    const baseURL = `https://unpkg.com/@ffmpeg/core@0.12.6/dist/${this.cfg.dist}`;
+    const ffmpeg = this.ffmpeg || new FFmpeg();
+
+    ffmpeg.on("log", ({ message }) => {
+      if (this.cfg.enableLogs) console.log(message);
+    });
+
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await //
+      toBlobURL(`${baseURL}/ffmpeg-core.wasm`, "application/wasm"),
+      workerURL: await //
+      toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, "text/javascript"),
+    });
+
+    this.isReady = true;
+  };
+
+  transcode = async ({
+    inputData,
+    ext,
+    id = nanoid(),
+    timeout,
+  }: TTranscode) => {
+    if (!this.isReady) await this.load();
+    if (!ext) throw new Error("Extension of the input video is required");
+
+    const ffmpeg = this.ffmpeg;
+    await ffmpeg.writeFile(`input-${id}.${ext}`, inputData);
+    await ffmpeg.exec(
+      ["-i", `input-${id}.${ext}`, `output-${id}.mp4`],
+      timeout
+    );
+    const data = await ffmpeg.readFile(`output-${id}.mp4`);
+    return data;
+  };
 }
